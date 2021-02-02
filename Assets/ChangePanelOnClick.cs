5ec304b95b295336fc1101efabd759709af28091
morita mai <mai.20.04.ab@gmail.com>
2021-02-02 21:45:17 +0900

    初期設定、オブジェクトの配置、パネル間のループ化、オブジェクトの表示/非表示機能を追加した。

---
+++ Assets/ChangePanelOnClick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//クリックした時に特定のパネルの場所に移動する
+
+public class ChangePanelOnClick : MonoBehaviour {
+
+    //右のボタンを押した時、x値が1500ずつ移動する
+    //左のボタンを押した時、x値が-1500ずつ移動する
+    //一番右（または左）に移動した時、矢印をなくす（ループでもいいかも）
+
+    int PANEL_DISTANCE = 1500;
+    int TOTAL_PANELS = 4;
+
+    public void RightArrow() {
+        Debug.Log("clicked");
+        Vector2 currentPosition = this.transform.localPosition;  //今いる場所
+        int currentX = (int)currentPosition.x;  //今いるX値
+        int panelNumber = -(currentX / PANEL_DISTANCE);  //1500分の1500 = 1となる
+        int nextPanelNumber = (panelNumber + 1) % TOTAL_PANELS;  //2割る4の余り(%)　あまりなので0,1,2,3のどれかにしかならない。
+        this.transform.localPosition = new Vector2(-(nextPanelNumber * PANEL_DISTANCE), currentPosition.y );
+    }
+
+    public void LeftArrow() {
+        Vector2 currentPosition = this.transform.localPosition;
+        int currentX = (int)currentPosition.x;
+        int panelNumber = -(currentX / PANEL_DISTANCE);
+        int nextPanelNumber = (panelNumber + TOTAL_PANELS - 1)%TOTAL_PANELS;
+        this.transform.localPosition = new Vector2(-(nextPanelNumber * PANEL_DISTANCE), currentPosition.y );
+    }//panelNumber=1の時は割る数字が0になってしまうため、TOTAL_PANELSを足す事で補う。
+
+}
---
+++ Assets/OnMouseDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouseDown : MonoBehaviour {
+
+    //クリックした時　オブジェクトを表示する
+
+    public string showObjectName;
+    GameObject showObject;
+
+    public void Start() {
+        showObject = GameObject.Find(showObjectName);
+        showObject.SetActive(false);
+    }
+
+    public void ShowPanel() {
+        showObject.SetActive(true);
+        Debug.Log("opened");
+    }
+
+    public void HidePanel() {
+        showObject.SetActive(false);
+        Debug.Log("closed");
+    }
+
+}
